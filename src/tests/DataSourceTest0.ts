0a6842ec4dc746d3a0c070d5b342f5f1dfc9d7ff
Sean Johnson <sjohnson@sjohnsonaz.com>
2016-12-22 13:23:53 -0700

    Created more DataSource tests.

--- src/tests/DataSourceTest0.ts
+++ src/tests/DataSourceTest0.ts
@@ -8,7 +8,7 @@ describe('DataSource', () => {
     for (let index = 0; index < 100; index++) {
         dataArray.push(index);
     }
-    it('Shows a page of data', () => {
+    it('should parse an array into a page of data', () => {
         var callCount = 0;
         var dataSource = new DataSource<number>((page, pageSize, sortedColumn, sortedDirection) => {
             callCount++;
@@ -23,4 +23,55 @@ describe('DataSource', () => {
             }
         });
     });
+
+    it('should parse an array into a second page of data', () => {
+        var callCount = 0;
+        var dataSource = new DataSource<number>((page, pageSize, sortedColumn, sortedDirection) => {
+            callCount++;
+            return DataSource.pageArray(dataArray, page, pageSize, sortedColumn, sortedDirection);
+        }, {
+                pageSize: 20
+            });
+        dataSource.init();
+        dataSource.page = 1;
+        Cascade.subscribe(dataSource, 'activeRows', (activeRows: number[]) => {
+            if (callCount === 2) {
+                expect(activeRows[0]).to.equal(10);
+            }
+        });
+    });
+
+    it('should return an empty array if the page is empty', () => {
+        var callCount = 0;
+        var dataSource = new DataSource<number>((page, pageSize, sortedColumn, sortedDirection) => {
+            callCount++;
+            return DataSource.pageArray(dataArray, page, pageSize, sortedColumn, sortedDirection);
+        }, {
+                pageSize: 20
+            });
+        dataSource.init();
+        dataSource.page = 5;
+        Cascade.subscribe(dataSource, 'activeRows', (activeRows: number[]) => {
+            if (callCount === 2) {
+                expect(activeRows.length).to.equal(0);
+            }
+        });
+    });
+
+    it('should parse an array into different size pages', () => {
+        var callCount = 0;
+        var dataSource = new DataSource<number>((page, pageSize, sortedColumn, sortedDirection) => {
+            callCount++;
+            return DataSource.pageArray(dataArray, page, pageSize, sortedColumn, sortedDirection);
+        }, {
+                pageSize: 20
+            });
+        dataSource.init();
+        dataSource.pageSize = 30;
+        Cascade.subscribe(dataSource, 'activeRows', (activeRows: number[]) => {
+            if (callCount === 2) {
+                expect(activeRows.length).to.equal(30);
+            }
+        });
+    })
 });
