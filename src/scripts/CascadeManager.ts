0f128b89493d6494a3fe8175d3503f0d9fc36c54
Sean Johnson <sjohnson@sjohnsonaz.com>
2017-01-11 15:01:12 -0700

    Fixed exports.

--- src/scripts/CascadeManager.ts
+++ src/scripts/CascadeManager.ts
@@ -6,13 +6,17 @@ export { ICrudConnection } from './interfaces/ICrudConnection';
 export { IData } from './interfaces/IData';
 export { IListQuery } from './interfaces/IListQuery';
 export { IListResult } from './interfaces/IListResult';
+export { IManager } from './interfaces/IManager';
 export { IModel } from './interfaces/IModel';
 export { IQueryModel } from './interfaces/IQueryModel';
 export { ISelectable } from './interfaces/ISelectable';
+export { IState } from './interfaces/IState';
 export { IStore } from './interfaces/IStore';
 
 export { default as Connection } from './implementations/Connection';
 export { default as CrudConnection } from './implementations/CrudConnection';
+export { default as Manager } from './implementations/Manager';
 export { default as Model } from './implementations/Model';
 export { default as QueryModel } from './implementations/QueryModel';
 export { default as Store } from './implementations/Store';
+export { default as State } from './implementations/State';
